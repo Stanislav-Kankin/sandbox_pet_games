0f2b936178a201d98d4a9c37998849170f1cf85f
Stnislav-Kankin <stanislav.kankin@yandex.ru>
2024-05-18 13:11:02 +0300

    добавил объект и научил его двигаться

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 import pygame
 
 pygame.init()
-screen = pygame.display.set_mode((600, 300))  # flags=pygame.NOFRAME(убираем рамку), задаем размеры экрана
+screen = pygame.display.set_mode((1000, 700))  # flags=pygame.NOFRAME(убираем рамку), задаем размеры экрана
 pygame.display.set_caption('Тестовое название для окошка')  # Задаем название для окошка
 icon = pygame.image.load('images/main_logo_pic.png')
 pygame.display.set_icon(icon)
 
+square = pygame.Surface((50, 170))  # Поверхность с заданными параметрами
+square.fill((97, 187, 199))
+
+my_font = pygame.font.Font('fonts/test_font.ttf', 48)
+text_surf = my_font.render('Random text', True, 'red')  # Русский не распознает шрифт
+
+player = pygame.image.load('images/man.png')
+
 running = True
+x = 100
+y = 0
 while running:
-    screen.fill((50, 168, 64))  # задний фон, заливка
+
+    screen.fill('white')
+    screen.blit(square, (20, 30))  # Рисуем обхект через blit
+    screen.blit(text_surf, (500, 200))
+    screen.blit(player, (x, y))
+    # pygame.draw.circle(screen, (30, 36, 89), (250, 50), 30)
 
     pygame.display.update()  # постоянное обновление приложения
 
@@ -17,5 +32,11 @@ while running:
             running = False
             pygame.quit()
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_a:  # отслеживаем клавиши
-                screen.fill((224, 103, 43))
+            if event.key == pygame.K_RIGHT:
+                x += 10
+            elif event.key == pygame.K_LEFT:
+                x -= 10
+            elif event.key == pygame.K_DOWN:
+                y += 10
+            elif event.key == pygame.K_UP:
+                y -= 10
